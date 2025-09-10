1589c5b4a56e354e8054bb2ab8537f041c4424b6
Reaktiv <itolmasov.olmasjon1505@gmail.com>
2025-09-10 17:34:42 +0500

    update

--- book/models.py
+++ book/models.py
@@ -8,6 +8,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     description = models.TextField(max_length=10000)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     photo = models.ImageField(upload_to='book_photos/')
     created_at = models.DateTimeField(auto_now_add=True)
     published = models.BooleanField(default=True)
